5f2a3f103855947fe798c9a45ac0c5ade051ecc8
VENKATESH BABU <154440420+venkatesh01-t@users.noreply.github.com>
2026-07-01 11:08:53 +0530

    Update models.py

--- clinic_manager_data/clinic/app/models.py
+++ clinic_manager_data/clinic/app/models.py
@@ -27,8 +27,7 @@ class Patient(models.Model):
     medical_history = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    new_patient = models.BooleanField(default=True)
-    last_name1 = models.CharField(max_length=255,blank=True, null=True)
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
